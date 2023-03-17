235de37ccb18f126b49dd0ae94a4c6995869a212
hoffmann michel <michel.hof@hotmail.fr>
2023-03-17 16:54:54 +0100

    style

--- .history/DOM_20230317163000.js
+++ .history/DOM_20230317163000.js
@@ -63,44 +63,4 @@ document.querySelector('h1').style.color = '#111d5e';
 document.querySelector('h1').style.textAlign = 'center';
 
 // 3ème méthode : avec une classe
-document.querySelector('header').className = "ma_class";
-Voici une fiche technique résumant l'ensemble des fonctions et propriétés principales liées au DOM.
-
-Accéder aux éléments
-getElementsByTagName() - Sélectionne tous les éléments avec la balise entre parenthèses
-
-getElementById() - Sélectionne un seul élément : le premier ayant l'ID entre parenthèses
-
-getElementsByClassName() - Sélectionne tous les éléments avec la classe entre parenthèses
-
-querySelector() - Sélectionne un seul élément : celui avec le sélecteur entre parenthèses
-
-querySelectorAll() - Sélectionne tous les éléments avec le sélecteur entre parenthèses
-
-
-
-Modifier les éléments
-textContent - Modifie le texte d'un élément
-
-innerHTML - Modifie l'HTML d'un élément
-
-
-
-Ajouter et supprimer des éléments
-createElement() - Crée un élément
-
-prepend() - Ajoute l'élément entre parenthèses devant l'élément cible
-
-append() - Ajouter l'élément entre parenthèses derrière l'élément cible (peut contenir du texte)
-
-appendChild() - Ajouter l'élément entre parenthèses derrière l'élément cible (ne peut pas contenir du texte)
-
-insertBefore() - Insère un élément avant l'élément cible
-
-
-
-Modifier le style d'un élément
-style.propriété - Modifie la propriété CSS spécifiée, par exemple : style.color = "orange"
-
-className - Modifie les classes d'un élément
------------------------------------------------------------------------------------------------------------------
+document.querySelector('header').className = "ma_class";Q
